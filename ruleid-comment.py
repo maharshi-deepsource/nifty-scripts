413578b6f5073c6990a494124b234b02de402824
Maharshi Basu <maharshi@deepsource.io>
2025-02-08 11:19:44 +0530

    add script to replace flag expection comments
    Signed-off-by: Maharshi Basu <maharshi@deepsource.io>

---
+++ ruleid-comment.py
@@ -0,0 +1,21 @@
+import os
+import re
+
+def replaceRuleidComments(filePath):
+    with open(filePath, "r") as f:
+        lines = f.readlines()
+    with open(filePath, "w") as f:
+        for line in lines:
+            # replace '# <expect-error>
+            line = re.sub(r"#\s*ruleid:\s*.*", "# <expect-error>", line)
+            line = re.sub(r"#\s*ok:\s*.*", "# <no-error>", line)
+            f.write(line)
+
+
+if __name__ == "__main__":
+    # works only on the current directory
+    for file in os.listdir():
+        # works on python files
+        if file.endswith(".py"):
+            replaceRuleidComments(file)
+            print(f"[INFO] Processed -> {file}")
